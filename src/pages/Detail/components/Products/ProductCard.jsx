d4f9b9692fce1401b8aa7a647df593b932e8bda5
Naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2023-08-15 15:21:42 +0700

    add product link

--- src/pages/Detail/components/Products/ProductCard.jsx
+++ src/pages/Detail/components/Products/ProductCard.jsx
@@ -3,7 +3,11 @@ import IdrFormat from "../../../../utils/IdrFormat";
 
 const ProductCard = ({ title, price, product_url, img_url }) => {
   return (
-    <div className="block bg-white rounded-md text-tp-dark w-28">
+    <a
+      href={product_url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block bg-white rounded-md text-tp-dark w-28">
       <img
         src={img_url}
         alt={title}
@@ -13,7 +17,7 @@ const ProductCard = ({ title, price, product_url, img_url }) => {
         <p className="line-clamp-1 text-[10px] mb-1">{title}</p>
         <p className="text-[10px] ">Rp {IdrFormat(price)}</p>
       </div>
-    </div>
+    </a>
   );
 };
 
